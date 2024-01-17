55a34a11dab5094faea0e67b39d5cccb795a62c0
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-18 01:17:30 +0200

    fix error handling

--- main.c
+++ main.c
@@ -18,14 +18,14 @@ int main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		perror("Usage: monty file\n");
+		fprintf(stderr, "Usage: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(av[1], "r");
 
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", av[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(line, sizeof(line), file) != NULL)
