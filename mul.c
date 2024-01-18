57f22865214f24fa0be1058f09025a2509e72d62
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-18 23:17:40 +0200

    mul

--- mul.c
+++ mul.c
@@ -11,6 +11,7 @@
 void mul(stack_t **head, int line_number)
 {
 	stack_t *top;
+	stack_t *second;
 	int result = 0;
 
 	if (*head == NULL || (*head)->next == NULL)
@@ -18,9 +19,9 @@ void mul(stack_t **head, int line_number)
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	top = (*head)->next;
-	result = (top->n) * (top->prev->n);
-	top->n = result;
-	free(top->prev);
-	top->prev = NULL;
+	top = *head;
+	second = top->next;
+	result = (top->n) * (second->n);
+	second->n = result;
+	pop(head, line_number);
 }
